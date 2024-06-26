010e8c9e4dafa84463665ed26932c31c9255bf76
Felipe Castrillon <felipecastrillon87@gmail.com>
2024-06-26 10:47:23 -0400

    organized agent.py added some requirements that were missing added some additional files to gitignore

--- src/agent.py
+++ src/agent.py
@@ -4,8 +4,9 @@ import vertexai
 import pdb
 import vertexai.preview.generative_models as generative_models
 from vertexai.preview.generative_models import GenerativeModel
+from vertexai.language_models import TextEmbeddingInput, TextEmbeddingModel
 from langchain.text_splitter import RecursiveCharacterTextSplitter
-from langchain_google_vertexai import VertexAIEmbeddings
+from langchain_community.embeddings.vertexai import VertexAIEmbeddings
 from langchain_community.document_loaders import PyPDFLoader
 from langchain_community.vectorstores import FAISS
 from langchain.schema.document import Document
@@ -19,52 +20,66 @@ class Agent:
   
   def __init__(self):
     
-    chunks = []
-    form_parser = None
-    form_processed = None  
+    self.form_parser = None
+    self.form_processed = None  
+    self.chunks = []
+    self.embed_model = None
+    self.llm_model = None
+    self.db = None
+
+    self.read_document()
+    self.get_embeddings()
+    self.create_retriever()
+    self.initialize_model()
+
+
+  def read_document(self):
 
     if (config.INCLUDE_TABLE_PARSING == True or config.INCLUDE_UNSTRUCTURED_TEXT_PARSING == True):
-      form_parser = DocAIParser(config.PROJECT, config.LOCATION, config.FORM_PROCESSOR_ID , config.FORM_PROCESSOR_VERSION) 
-      form_processed = form_parser.process_document()
+      self.form_parser = DocAIParser(config.PROJECT, config.LOCATION, config.FORM_PROCESSOR_ID , config.FORM_PROCESSOR_VERSION) 
+      self.form_processed = self.form_parser.process_document()
     
-    # if document has tables
+    # if document has tables that you want to parse
     table_indexes = []
     if (config.INCLUDE_TABLE_PARSING == True):
-      tables, table_indexes = form_parser.read_tables(form_processed)
+      tables, table_indexes = self.form_parser.read_tables(self.form_processed)
       for table in tables:
         doc =  Document(page_content=table, metadata={"source": "local"})
-        chunks.append(doc) 
+        self.chunks.append(doc) 
     
-    # if document has unstructured text 
+    # if document has unstructured text that you want to parse
     if (config.INCLUDE_UNSTRUCTURED_TEXT_PARSING == True):
-      unstructured_text = form_parser.read_text(form_processed, config.INCLUDE_TABLE_PARSING, table_indexes)
+      unstructured_text = self.form_parser.read_text(self.form_processed, config.INCLUDE_TABLE_PARSING, table_indexes)
       text_splitter = RecursiveCharacterTextSplitter(chunk_size = config.CHUNK_SIZE,
                                            chunk_overlap=config.CHUNK_OVERLAP)
-      chunks += text_splitter.create_documents([unstructured_text])    
+      self.chunks += text_splitter.create_documents([unstructured_text])    
      
-    # if document has entities you want to extract 
+    # if document has specific entities you want to extract 
     if (config.INCLUDE_ENTITY_PARSING == True):
       cde_parser = DocAIParser(config.PROJECT, config.LOCATION, config.CDE_PROCESSOR_ID , config.CDE_PROCESSOR_VERSION) 
       cde_processed = cde_parser.process_document()
       entities = cde_parser.read_entities(cde_processed)
       for entity in entities:
         doc = Document(page_content=entity, metadata={"source" : "local"})
-        chunks.append(doc)
+        self.chunks.append(doc)
+
+  def get_embeddings(self):
 
     # create embeddings from chunks
-    embed_model = None
     if config.EMBEDDINGS_MODEL == "google-gecko":
-      embed_model= VertexAIEmbeddings()
+      self.embed_model= VertexAIEmbeddings(model_name="textembedding-gecko")
     else:
       raise ValueError("splitter input must be one of the following values [\"google-gecko\"]") 
-    
+  
+  def create_retriever(self):
     # create retriever 
     db = None
     if config.RETRIEVER_DB == "faiss":
-      self.db = FAISS.from_documents(chunks, embed_model)         
+      self.db = FAISS.from_documents(self.chunks, self.embed_model)         
     else:
       raise ValueError("splitter input must be one of the following values [\"faiss\"]") 
-    
+  
+  def initialize_model(self):
     # initialize genai model
     self.llm_model = ""
     if config.LLM_MODEL == "gemini":
